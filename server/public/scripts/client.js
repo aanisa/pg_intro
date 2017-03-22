d8edff6628ea3f4f79b815c616641445584b81c9
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-22 15:33:36 -0500

    delete adn edit button append only no function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,43 +1,86 @@
+var editing = false;
+var bookID = 0;
+
 $(document).ready(function() {
-  console.log("jQ sourced");
+    console.log("jQ sourced");
 
-  $('#bookForm').on('submit', function(event) {
-    event.preventDefault();
-    var title = $('#title').val();
-    var author = $('#author').val();
-    var year = $('#year').val();
-    var publisher = $('#publisher').val();
-    console.log(title, author, year, publisher);
-
-    $.ajax({
-      type: "POST",
-      url: "/books/add",
-      data: {title: title, author: author, year: year, publisher: publisher},
-      success: function(response) {
-        console.log(response);
-        getBooks();
-      }
+    $('#books').on('click', '.delete', function() {
+        console.log('Delete' + $(this).data('book')); ///able to delete book with specific id. check to make sure it's refering to right id
+        // $.ajax({
+        //   type: 'DELETE',
+        //   url: books/delete + id
+        // });
     });
-  });
-getBooks();
+
+
+    $('#books').on('click', '.edit', function() { //stashing this data so it can fill in information later
+        editing = true;
+        ('#formTitle').text("you are now editing...");
+        // console.log($(this).data('book'));
+        // console.log($(this).data('author'));
+        // console.log($(this).data('title'));
+        $('#title').val($(this).data('title')); //immediately adds author name into input field
+        $('#author').val($(this).data('author'));
+    });
+
+    $('#bookForm').on('submit', function(event) {
+        event.preventDefault();
+        var title = $('#title').val();
+        var author = $('#author').val();
+        var year = $('#year').val();
+        var publisher = $('#publisher').val();
+        console.log(title, author, year, publisher);
+
+        if (editing) {
+          editing = false;
+          $('#formTitle').text("Add new entry");
+            // $.ajax
+            // type: 'PUT', //to UPDATE the DB. use data object and req.body not url
+
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "/books/add",
+                data: {
+                    title: title,
+                    author: author,
+                    year: year,
+                    publisher: publisher
+                },
+                success: function(response) {
+                    console.log(response);
+                    getBooks();
+                }
+            });
+        }
+        $('#title').val('');
+        $('#author').val(''); //empty inputs
+    });
+
+    getBooks();
 });
 
 function getBooks() {
-  $.ajax({
-    type: "GET",
-    url: "/books",
-    success: function(response) {
-      // $('#books').empty();
-      for (var i = 0; i < response.length; i++) {
-      var book =  response[i];
-      $('#books').append('<tr></tr>');
-      var $el = $('#books').children().last();
-      $el.append('<td>' + book.id + '</td>');
-      $el.append('<td>' + book.author + '</td>');
-      $el.append('<td>' + book.title + '</td>');
-      $el.append('<td>' + book.year + '</td>');
-      $el.append('<td>' + book.publisher + '</td>');
-      }
-    }
+    $.ajax({
+        type: "GET",
+        url: "/books",
+        success: function(response) {
+            // $('#books').empty();
+            $('#books').empty();
+            for (var i = 0; i < response.length; i++) {
+                var book = response[i];
+                $('#books').append('<tr></tr>');
+                var $el = $('#books').children().last();
+                $el.append('<td>' + book.id + '</td>');
+                $el.append('<td>' + book.author + '</td>');
+                $el.append('<td>' + book.title + '</td>');
+                $el.append('<td>' + book.year + '</td>');
+                // $el.append('<td>' + book.publisher + '</td>');
+                $el.append('<td><button class="delete" data-book="' +
+                    book.id + '">Delete</button></td>');
+                $el.append('<td><button class="edit" data-book="' + book.id +
+                    '" data-author="' + book.author + '" data-title="' + book.title + '" >Edit</button></td>');
+            }
+        }
     });
 }
